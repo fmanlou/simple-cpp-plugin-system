21964bae642c2a25f6c5b9d37598f790b3b3f850
fmanlou <2632335513@qq.com>
2024-01-01 19:04:56 +0800

    将PluginSystem变量置为全局变量

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include "pluginsystem.h"
 
+IPluginSystem *gPluginSystem{nullptr};
+
 int main() {
   PluginSystem pluginSystem;
+
+  gPluginSystem = &pluginSystem;
+
   pluginSystem.loadPlugins();
   return 0;
 }
